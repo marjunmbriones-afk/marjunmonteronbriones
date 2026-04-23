f074827c8c884ab2e173483f2fa0553a25161663
Marjun Monteron Briones <marjunmbriones@gmail.com>
2026-04-23 16:49:50 +0800

    Update 1_👤_About.py

--- portfolio_app/pages/1_👤_About.py
+++ portfolio_app/pages/1_👤_About.py
@@ -6,7 +6,7 @@ st.title("👤 About Me")
 col1, col2 = st.columns([1, 2])
 
 with col1:
-    st.image("profile.jpg", width=180)
+    st.image("portfolio_app/marjun", width=180)
 
 with col2:
     st.markdown("""
